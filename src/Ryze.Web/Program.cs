16ef0e659f51a76db45eed874fe5afbe4827d312
EoBread <joao.alencar099@gmail.com>
2025-04-30 00:14:07 -0300

    feat: Configure JWT authentication and authorization policies in Program.cs

--- src/Ryze.Web/Program.cs
+++ src/Ryze.Web/Program.cs
@@ -1,21 +1,45 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Ryze.Domain.Entities;
+using Ryze.Domain.Enums;
 using Ryze.Infrastructure;
 using Ryze.Infrastructure.Database;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<RyzeDbContext>(c =>
-{
-    c.UseNpgsql();
-});
+builder.Services.AddDbContext<RyzeDbContext>(c => { c.UseNpgsql(); });
 
 builder.Services.AddInfrastructure(builder.Configuration);
+
+var key = builder.Configuration.GetSection("Jwt:Key").Value;
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!))
+        };
+    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy(nameof(UserRoles.GlobalAdmin), policy => policy.RequireRole(nameof(UserRoles.GlobalAdmin)))
+    .AddPolicy(nameof(UserRoles.Manager), policy => policy.RequireRole(nameof(UserRoles.GlobalAdmin)))
+    .AddPolicy(nameof(UserRoles.Seller), policy => policy.RequireRole(nameof(UserRoles.GlobalAdmin)));
 
 var app = builder.Build();
 
