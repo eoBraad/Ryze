b54fe88fbd78e7b128b75b46f0603278f9589806
Joao Pontes <joao.pontes@software.com.br>
2025-05-16 16:47:08 -0300

    feat: add ProductController with CreateProduct endpoint

---
+++ src/Ryze.Web/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ryze.Application.Services.Product.CreateProduct;
+using Ryze.Application.Services.Product.CreateProduct.Dto;
+
+namespace Ryze.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct([FromBody] CreateProductDtoRequest request, [FromServices] CreateProductService service)
+    {
+        await service.CreateProduct(request);
+        
+        return Created("", null);
+    }
+}
