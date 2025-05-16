ef68423fdb5036f8bbd092f5a3b55937e91bf3af
Joao Pontes <joao.pontes@software.com.br>
2025-05-16 15:51:31 -0300

    refactor: update Company and Product models to use nullable properties

--- src/Ryze.Domain/Entities/Company.cs
+++ src/Ryze.Domain/Entities/Company.cs
@@ -4,17 +4,19 @@ public class Company : BaseEntity
 {
     public string CompanyName { get; set; } = string.Empty;
 
-    public string CompanyPhone { get; set; } = string.Empty;
+    public string? CompanyPhone { get; set; }
 
-    public int CompanyEmployees { get; set; }
+    public int? CompanyEmployees { get; set; }
+    
+    public string? CompanyLogoUrl { get; set; } 
 
-    public string CompanyAddress { get; set; } = string.Empty;
+    public string? CompanyAddress { get; set; } 
 
-    public decimal CompanyRevenuePerYear { get; set; }
+    public decimal? CompanyRevenuePerYear { get; set; }
 
-    public string CompanyWebsite { get; set; } = string.Empty;
+    public string? CompanyWebsite { get; set; } 
 
-    public string CompanyDescription { get; set; } = string.Empty;
+    public string? CompanyDescription { get; set; } 
 
-    public string CompanyCnpj { get; set; } = string.Empty;
+    public string? CompanyCnpj { get; set; }
 }
--- src/Ryze.Domain/Entities/Product.cs
+++ src/Ryze.Domain/Entities/Product.cs
@@ -4,6 +4,6 @@ public class Product : BaseEntity
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public decimal Price { get; set; }
+    public decimal? Price { get; set; }
     public List<Lead?> Leads { get; set; }
 }
